fddd6089f6add4001627239819b543c5d24b7a01
Tim Lauridsen <timlau@fedoraproject.org>
2011-01-31 16:07:36 +0100

    Added bugzilla bug handling

--- src/reviewtools/bugzilla.py
+++ src/reviewtools/bugzilla.py
@@ -23,3 +23,65 @@ from bugzilla import Bugzilla
 
 BZ_URL='https://bugzilla.redhat.com/xmlrpc.cgi'
 
+from reviewtools import Helpers
+
+class ReviewBug(Helpers):
+    def __init__(self,bug,user=None,password=None):
+        self.bug_num = bug
+        self.spec_url = None
+        self.srpm_url = None
+        self.spec_file = None
+        self.srpm_file = None
+        self.bugzilla = Bugzilla(url=BZ_URL)
+        self.login = False
+        if user and password:
+            self.bugzilla.login(user=user, password=password)
+            self.login = True
+        self.user = user
+        self.bug = self.bugzilla.getbug(self.bug_num)
+
+    def find_urls(self):
+        if self.bug.longdescs:
+            for c in self.bug.longdescs:
+                body = c['body']
+                urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', body)
+                if urls:
+                    for url in urls:
+                        if url.endswith(".spec"):
+                            self.spec_url = url
+                        elif url.endswith(".src.rpm"):
+
+                            self.srpm_url = url
+
+    def assign_bug(self):    
+        if self.login:
+            self.bug.setstatus('ASSIGNED')
+            self.bug.setassignee(assigned_to=self.user)
+            self.bug.addcomment('I will review this bug')
+            flags = {'fedora-review' : '?'}
+            self.bug.updateflags(flags)
+            self.bug.addcc([self.user])
+        else:
+            print("You need to login before assigning a bug")
+
+    def add_comment(self,comment):    
+        if self.login:
+            self.bug.addcomment(comment)
+        else:
+            print("You need to login before commenting on a bug")
+
+    def add_comment_from_file(self,fname):
+        fd = open(fname,"r")
+        lines = fd.readlines()
+        fd.close
+        self.add_comment("".join(lines))
+                            
+    def download_files(self):
+        if self.spec_url and self.srpm_url:
+            self.spec_file = self._get_file(self.spec_url)
+            self.srpm_file = self._get_file(self.srpm_url)
+            if self.spec_file and self.srpm_file:
+                return True
+        return False
+
+    
