4c55f3aa4931e3a4743e77cd0eea8c54c2f94514
Pierre-Yves Chibon <pingou@pingoured.fr>
2011-10-07 14:43:50 +0200

    Fix shebang, add encoding and remove last empty line

--- src/reviewtools/checks/generic.py
+++ src/reviewtools/checks/generic.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python -tt
+#-*- coding: UTF-8 -*-
+
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
