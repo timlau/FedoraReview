fdc71e9e0044a267617f32bbbc0bd742348d146f
Pierre-Yves Chibon <pingou@pingoured.fr>
2011-10-07 17:51:52 +0200

    New R specific checks

--- src/reviewtools/checks/R.py
+++ src/reviewtools/checks/R.py
@@ -1,5 +1,7 @@
 #-*- coding: UTF-8 -*-
 
+import re
+import os
 from generic import LangCheckBase, CheckBase
 
 
@@ -30,3 +32,49 @@ class RCheckBuildRequires(RCheckBase):
         br = self.spec.find_tag('BuildRequires')
         self.set_passed('R-devel' in br and 'tex(latex)' in br)
 
+
+class RCheckRequires(RCheckBase):
+    """ Check if the Requires have R-core. """
+
+    def __init__(self, base):
+        """ Instanciate check variable """
+        CheckBase.__init__(self, base)
+        self.url = 'http://fedoraproject.org/wiki/Packaging:R'
+        self.text = 'Package requires R-core.'
+        self.automatic = True
+
+    def run(self):
+        """ Run the check """
+        br = self.spec.find_tag('BuildRequires')
+        if 'R ' in br and not 'R-core':
+            self.type= "SHOULD"
+            self.text= "Package should requires R-core rather than R"
+            self.set_passed(False)
+        else:
+            self.set_passed('R-core' in br)
+
+
+class RCheckDoc(RCheckBase):
+    """ Check if the package has the usual %doc. """
+
+    def __init__(self, base):
+        """ Instanciate check variable """
+        CheckBase.__init__(self, base)
+        print self.spec.find_tag('packname')
+        self.doc = []
+        for f in ['doc','DESCRIPTION','NEWS', 'CITATION']:
+            if self.has_files("*" + f):
+                self.doc.append(f)
+        self.url = 'http://fedoraproject.org/wiki/Packaging:R'
+        self.text = 'Package have the default element marked as %%doc : %s' % (
+        ", ".join(self.doc))
+        self.automatic = True
+
+    def run(self):
+        """ Run the check """
+        br = self.spec.find_all(re.compile("%doc.*"))
+        for entry in br:
+            entry = os.path.basename(entry.group(0)).strip()
+            if str(entry) in self.doc:
+                self.doc.remove(entry)
+        self.set_passed(self.doc == [])
