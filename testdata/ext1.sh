9c65e5e8d42c778039f0082cba087868c403c749
Stanislav Ochotnicky <sochotnicky@redhat.com>
2011-11-07 17:36:59 +0100

    Add testing of get_section to test_data shellscript

--- testdata/ext1.sh
+++ testdata/ext1.sh
@@ -7,17 +7,29 @@ while read;do
 done
 
 echo '{"supported_api":1,
+    "command":"get_section",
+    "section":"build"
+}'
+
+while read;do
+    if [ "$REPLY" == "" ];then
+        break
+    fi
+    echo $REPLY > /tmp/get_section
+done
+
+echo '{"supported_api":1,
 "command":"results",
 "checks":[
     {
     "name":"ExtShellTest",
     "url":"http://nonsensical.url",
-    "group":"Shell",
+    "group":"Java",
     "deprecates":[],
     "text":"Verify external plugins work OK",
     "type":"MUST",
     "result":"fail",
-    "extra_output":"Ext plugins work!"
+    "output_extra":"Ext plugins work!"
     }
 ]
 }'
