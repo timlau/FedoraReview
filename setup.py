179ace116f9d6e65a45259ad805ef2b8ce4231b8
Stanislav Ochotnicky <sochotnicky@redhat.com>
2011-11-10 20:31:16 +0100

    Little tweaks to setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ from distutils.core import setup
 
 setup(
     name = 'FedoraReview',
-    description = 'Tools to helper review packages for inclusion in Fedora',
+    description = 'Tools to help review packages for inclusion in Fedora',
     description_long = '',
-    data_files = [('/usr/share/man/man1', [ 'fedora-review.1' ] ) ],
+    data_files = [('/usr/share/man/man1/', [ 'fedora-review.1' ] ) ],
     version = '0.1.0',
     license = 'GPLv2+',
-    url = 'https://fedorahosted.org/releases/F/e/FedoraReview/',
-    download_url = 'https://fedorahosted.org/FedoraReview/',
+    download_url = 'https://fedorahosted.org/releases/F/e/FedoraReview/',
+    url = 'https://fedorahosted.org/FedoraReview/',
     package_dir = {'FedoraReview': 'src/FedoraReview'},
     packages = ['FedoraReview', 'FedoraReview.checks'],
+    scripts=["src/fedora-review"],
     )
