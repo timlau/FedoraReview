ef20377f0adc77218361b1672df79299702f263f
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-01-13 14:43:05 +0100

    Add man page for fedora-create-review and fix setup.py accordingly

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name = 'fedora-review',
     description = 'Tools to help review packages for inclusion in Fedora',
     description_long = '',
-    data_files = [('/usr/share/man/man1/', [ 'fedora-review.1' ] ) ],
+    data_files = [('/usr/share/man/man1/', [ 'fedora-review.1', 'fedora-create-review.1' ] ) ],
     version = __version__,
     license = 'GPLv2+',
     download_url = 'https://fedorahosted.org/releases/F/e/FedoraReview/',
