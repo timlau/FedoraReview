e5e04380186a4078a35fee9a1f01c7458f7b27c4
Alec Leamas <alec@tests.notat.diaspora.com>
2012-09-05 10:06:29 +0200

    shell-api: add attachments, clear environment
    Att new attachment function + some tests. Clear the environment
    before invoking shell based tests.

--- scripts/java-check-bundled-jars.sh
+++ scripts/java-check-bundled-jars.sh
@@ -6,17 +6,8 @@
 # @text: Bundled jar/class files should be removed before build
 
 cd BUILD
-jars="$( find . -name \*.jar -o -name \*.class )"
+jars="$jars $( find . -name \*.jar -o -name \*.class )"
 test -z "$jars" && exit $FR_PASS
-count=$( echo $jars | wc -w)
-cd ..
-if (( ${#jars} < 100 )); then
-    echo "$count jar or class files(s) in source (see attachment)"
-    echo  "$jars" | attach 8 "Jar and class files in source"
-else
-    echo "$count jar or class(s) file in source, see file bundled-jars.txt"
-    for jar in $jars; do
-        echo $jar
-    done > 'bundled-jars.txt'
-fi
+echo "Jar files in source (see attachment)"
+echo  "$jars" | attach 8 "Jar and class files in source"
 exit $FR_FAIL
