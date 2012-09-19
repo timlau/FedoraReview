637ee8e9d1f9945ac131dc195bce6961eb150feb
Alec Leamas <alec@tests.notat.diaspora.com>
2012-09-19 09:34:06 +0200

    Fix last pylint warnings (#132)

--- plugins/sugar_activity.py
+++ plugins/sugar_activity.py
@@ -15,8 +15,12 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # (C) 2012 - Michael Scherer <misc@fedoraproject.org>
+''' Sugar tests plugin module. '''
 
 from FedoraReview import CheckBase, RegistryBase
+
+_SUGAR_URL = \
+    'https://fedoraproject.org/wiki/Packaging:SugarActivityGuidelines#'
 
 
 class Registry(RegistryBase):
@@ -29,6 +33,7 @@ class Registry(RegistryBase):
 
 
 class SugarActivityCheckBase(CheckBase):
+    ''' Common base class for sugar checks. '''
 
     def __init__(self, base):
         CheckBase.__init__(self, base, __file__)
@@ -38,7 +43,7 @@ class SugarActivityCheckNaming(SugarActivityCheckBase):
     ''' All activities MUST be named sugar-<activity name>  '''
     def __init__(self, base):
         SugarActivityCheckBase.__init__(self, base)
-        self.url = 'https://fedoraproject.org/wiki/Packaging:SugarActivityGuidelines#Naming'
+        self.url = _SUGAR_URL + 'Naming'
         self.text = 'Sugar activities must be named sugar-<activity name>'
         self.type = 'MUST'
         self.automatic = True
@@ -47,7 +52,8 @@ class SugarActivityCheckNaming(SugarActivityCheckBase):
         if not self.spec.name.startswith('sugar-'):
             self.set_passed(False)
             return
-        # TODO check if sugar-foo is valid or if there is specific need for activity name
+        # TODO check if sugar-foo is valid or if there is specific
+        # need for activity name
         self.set_passed('inconclusive')
 
 
@@ -55,7 +61,7 @@ class SugarActivityCheckBuildRequires(SugarActivityCheckBase):
     ''' All activities requires sugar-toolkit to build '''
     def __init__(self, base):
         SugarActivityCheckBase.__init__(self, base)
-        self.url = 'https://fedoraproject.org/wiki/Packaging:SugarActivityGuidelines#Necessary_BuildRequires'
+        self.url = _SUGAR_URL + 'Necessary_BuildRequires'
         self.text = 'Sugar activities depend on sugar-toolkit'
         self.type = 'MUST'
         self.automatic = True
@@ -69,7 +75,8 @@ class SugarActivityCheckRuntimeDeps(SugarActivityCheckBase):
     ''' All runtime dependency information MUST be manually added. '''
     def __init__(self, base):
         SugarActivityCheckBase.__init__(self, base)
-        self.url = 'https://fedoraproject.org/wiki/Packaging:SugarActivityGuidelines#Runtime_Dependencies'
-        self.text = 'All runtime dependency information need to be manually added.'
+        self.url = _SUGAR_URL + 'Runtime_Dependencies'
+        self.text = 'All runtime dependency information need to be' \
+                    ' manually added.'
         self.type = 'MUST'
         self.automatic = False
