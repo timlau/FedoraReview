9afee6c10e797af35d0997a217ef89f0607968a0
Alec Leamas <alec@tests.notat.diaspora.com>
2012-10-28 00:04:17 +0200

    srcpath: bugfixes

--- test/srcpath.py
+++ test/srcpath.py
@@ -1,10 +1,29 @@
+#-*- coding: utf-8 -*-
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#    MA 02110-1301 USA.
+#
+''' Insert path to package into sys.path. '''
+
 import os
 import sys
 
+from distutils.sysconfig import get_python_lib
+
 if os.path.exists('../src'):
-    srcpath = '../src'
-elif os.path.exists('../../../lib/python2.7/site-packages/FedoraReview/src'):
-    srcpath = '../../../lib/python2.7/site-packages/FedoraReview/src'
+    SRC_PATH = os.path.abspath('../src')
 else:
-    assert False, "Can't find src path"
-sys.path.insert(0, os.path.abspath(srcpath))
+    SRC_PATH = os.path.join(get_python_lib(), 'FedoraReview')
+    assert os.path.exists(SRC_PATH), "Can't find src path"
+sys.path.insert(0, SRC_PATH)
