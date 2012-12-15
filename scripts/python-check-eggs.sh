b07f764c0ff2548510d6a728ec8613b41fe5b539
Alec Leamas <leamas.alec@gmail.com>
2012-12-15 14:26:24 +0100

    python-check-eggs.sh: Don't use FR_NAME, source could have any name

--- scripts/python-check-eggs.sh
+++ scripts/python-check-eggs.sh
@@ -3,12 +3,7 @@
 # @url: http://fedoraproject.org/wiki/Packaging:Python#Packaging_eggs_and_setuptools_concerns
 # @text: Binary eggs must be removed in %prep
 
-if [ ! -d BUILD/${FR_NAME}-*  ]; then
-    echo "Cannot find sources under BUILD (using prebuilt sources?)"
-    exit $FR_PENDING
-fi
-
-cd BUILD/${FR_NAME}-*
+cd BUILD/*
 eggs=$( find . -name \*.egg)
 if [ -n "$eggs" ]; then
     echo "Binary egg files not removed in %prep: $eggs"
