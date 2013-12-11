178b7b77929119e73f693aa2aca5dab1c68e8252
Alec Leamas <alec@tests.notat.diaspora.com>
2013-12-11 14:53:23 +0100

    generic_should: EPEL5: fix buildroot check text (bz 1040353)

--- plugins/generic_should.py
+++ plugins/generic_should.py
@@ -77,6 +77,9 @@ class CheckBuildroot(GenericShouldCheckBase):
         self.url = 'http://fedoraproject.org/wiki/' \
                    'Packaging/Guidelines#BuildRoot_tag'
         self.text = 'Buildroot is not present'
+        if self.flags['EPEL5']:
+            self.text = \
+                "Explicit BuildRoot: tag as required by EPEL5 present."
         self.automatic = True
         self.type = 'SHOULD'
 
